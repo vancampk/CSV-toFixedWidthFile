b815ed76d4424314e51771b7d9c17fbb2786fb1d
vancampk <vancampk89@gmail.com>
2023-03-10 08:47:36 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CSVToFixedWidth
+{
+    using System.Text;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = args.ToList().First() ?? string.Empty;
+		   string output = args.ToList().Last() ?? string.Empty;
+			if(File.Exists(input)){
+				List<string> lines =  File.ReadAllLines(input).ToList();
+				StringBuilder sb = new StringBuilder();
+
+				foreach(string line in lines){
+					List<string> segments = line.Split(",").ToList();
+					string newLine = string.Empty;
+					
+					foreach(string str in segments){
+						newLine += str.Trim().PadRight(9, ' ' );
+					}
+
+					sb.AppendLine(newLine);					
+				}
+				 File.WriteAllText(output, sb.ToString());
+				
+			}
+			else{
+				Console.WriteLine("Input file could not be found!");
+			}
+        }
+    }
+}
